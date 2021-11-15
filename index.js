8b639cbebdc7b5c9be153644d4574e015efbad50
RBar70 <R.barber@utah.edu>
2021-11-14 19:04:52 -0700

    fingers crossed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ for (var i = 0; i <101; i++) {
     if (i === 0) {
             console.log(i +  " is even");
     }
-    else if (i % 2 === 0) {
+    else if (i %2 === 0) {
             console.log(i + " is even");   
     }
     else {
